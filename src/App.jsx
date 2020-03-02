3ef018ac0ceebec4562db6ee0135ad339a053af7
Brent Nolan <brentnolan@Brents-MacBook-Pro.local>
2020-03-01 23:40:47 -0600

    Christmas Countdown

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css'
+import Clock from './clock'
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2020',
+            newDeadline: ''
+        }
+    }
+
+    changeDeadline(){
+        this.setState({deadline: this.state.newDeadline})
+    }
+
+    render(){
+        return (
+            <div className="App"> 
+                <div className="App-title">Countdown to {this.state.deadline}</div>
+                <div>
+                    <Clock 
+                        deadline={this.state.deadline}
+                    />
+                </div>
+                <div>
+                    <input 
+                        placeholder='new date'
+                        onChange={event => this.setState( {newDeadline: event.target.value} )}
+                    />
+                    <button onClick={() => this.changeDeadline()}>Submit</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
